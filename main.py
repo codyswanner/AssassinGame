5323cfac2646a9a2d78830cdf5580e038b8ebb99
Cody Swanner <108250848+codyswanner@users.noreply.github.com>
2023-05-13 20:51:01 -0500

    Created some functions to clear up the main logic flow.

--- main.py
+++ main.py
@@ -8,11 +8,56 @@
 """
 import random
 
+###################################################
+# functions
+
+def reveal_target(assassin):
+    if assassin in players:
+            assassin_index = players.index(assassin)
+            target_index = assassin_index + 1
+            if target_index == len(players):
+                target_index = target_index - len(players)
+            print(assassin + "'s target is: " + players[target_index])
+    else:
+        print("Error: that is not a valid player name!")
+
+def eliminate_player(target):
+    if target in players:
+            target_index = players.index(target)
+            players.remove(target)
+            reveal_next_target = input("Would you like to reveal your next target? (y/n): ")
+            while reveal_next_target.lower() not in ("y", "n"):
+                print("Please enter 'y' or 'n' only (without quotes)")
+                reveal_next_target = input("Would you like to reveal your next target? (y/n): ")
+            if reveal_next_target == "y":
+                print("Your next target is: " + players[target_index])
+            else:
+                print("That is not a valid player name!")
+
+def show_remaining_players():
+    display_players = players
+    random.shuffle(display_players)
+    print(display_players)
+
+def show_help():
+    available_commands = (
+        "reveal target -- shows the target of a given player",
+        "remaining players -- shows who is still in the game",
+        "eliminate target -- removes target from the game and optionally shows player's next target")
+    print("Available commands are:")
+    for command in available_commands:
+        print(command)
+
+# end functions
+###################################################
+
 f = open("players_list.txt", "w+")
 
 next_player = ""
 players = []
 
+# TODO: create a "add player" function
+# TODO: prevent entry of empty string as a player name (if user hits enter without text, prompt them to type "done" to finish player entry)
 while next_player.lower() != "done":
     next_player = input("Name of player: ")
     if next_player.lower() == "list players":
@@ -23,45 +68,29 @@ while next_player.lower() != "done":
     else:
         players.append(next_player)
 
+# Shuffle the players into a random list
+# The player below you on the list is your target
 random.shuffle(players)
 
+# wrap into user input handler function?
 user_input = ""
 while user_input != "exit":
     user_input = input("Enter command: ")
+    
     if user_input.lower() == "reveal target":
         assassin = input("Whose target should be revealed?  ")
-        if assassin in players:
-            assassin_index = players.index(assassin)
-            target_index = assassin_index + 1
-            if target_index == len(players):
-                target_index = target_index - len(players)
-            print(assassin + "'s target is: " + players[target_index])
-        else:
-            print("Error: that is not a valid player name!")
+        reveal_target(assassin)
 
     elif user_input.lower() == "eliminate player":
         target = input("Who is elimated?  ")
-        if target in players:
-            target_index = players.index(target)
-            players.remove(target)
-            reveal_next_target = input("Would you like to reveal your next target? (y/n): ")
-            while reveal_next_target.lower() not in ("y", "n"):
-                print("Please enter 'y' or 'n' only (without quotes)")
-                reveal_next_target = input("Would you like to reveal your next target? (y/n): ")
-            if reveal_next_target == "y":
-                print("Your next target is: " + players[target_index])
-
-        else:
-            print("That is not a valid player name!")
+        eliminate_player(target)
 
     elif user_input.lower() == "remaining players":
-        display_players = players
-        random.shuffle(display_players)
-        print(display_players)
+        show_remaining_players()
+
     elif user_input.lower() == "help" or user_input.lower() == '"help"':
-        print("Available commands are:")
-        print("reveal target -- shows the target of a given player")
-        print("remaining players -- shows who is still in the game")
+        show_help()
+
     elif user_input.lower() == "exit":
         break
     else:
